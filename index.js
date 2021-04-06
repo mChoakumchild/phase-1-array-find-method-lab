8fc2fdb41e88862b1ea98672f16ddf95514c5ca4
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-04-06 00:13:30 -0400

    commit tree 80e6d719ba4fc0f5f745bba1f1720a87925ad900

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function date(element,index,array){
+    let a = array[index].year;
+    if (array[index].result == 'W'){
+        //console.log(a);
+        return a; 
+    }
+    
+    
+ }
+function superbowlWin(record){
+   let a = record.find(date); //.find method returns either the whole element ({ year: '1969', result: 'W' }) or undefined.
+   if (a != undefined){
+    return a.year;
+   }
+   else{
+       return a;
+   }
+}
 
